7333dcbadb115b5f557af7596645926442d1e813
Tchoupinax <corentinfiloche@hotmail.fr>
2020-09-05 12:42:50 +0200

    style: allow to show all repo on list view (css)

--- pages/list.vue
+++ pages/list.vue
@@ -10,7 +10,7 @@
 
     <div class="flex justify-center">
       <div class="w-full lg:w-8/12">
-        <div class="flex flex-col items-center justify-center text-theme">
+        <div class="flex flex-col items-center justify-center mb-40 text-theme">
           <p class="mb-24 text-4xl">
             {{ url }}
           </p>
