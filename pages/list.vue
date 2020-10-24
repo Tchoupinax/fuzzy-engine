1c4e5176f2565da8376bd2b32e615423e057595d
Tchoupinax <corentinfiloche@hotmail.fr>
2020-10-24 04:39:17 +0200

    feat: remove empty repository from the list

--- pages/list.vue
+++ pages/list.vue
@@ -102,6 +102,12 @@
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.88 9.88l-3.29-3.29m7.532 7.532l3.29 3.29M3 3l3.59 3.59m0 0A9.953 9.953 0 0112 5c4.478 0 8.268 2.943 9.543 7a10.025 10.025 0 01-4.132 5.411m0 0L21 21" />
                 </svg>
               </button>
+
+              <button v-show="hiddingRepoMode" class="w-6 ml-2">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                  <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd" />
+                </svg>
+              </button>
             </div>
           <!-- END # Right -->
           </div>
@@ -145,7 +151,10 @@ export default {
         });
     }));
 
-    return { repositories };
+    // Remove empty repository from the list
+    const filteredRepositories = repositories.filter(r => r.countOfTags > 0);
+
+    return { repositories: filteredRepositories };
   },
   data () {
     return {
