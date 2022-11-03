fefff5c9dc89e3bedc3237e71c43eaff51141106
Tchoupinax <corentinfiloche@hotmail.fr>
2022-11-04 00:41:30 +0100

    feat: repair tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,25 +8,23 @@ import { colors } from 'tailwindcss/defaultTheme'
 
 module.exports = {
   theme: {
-    colors: {
-      ...colors,
-      theme: {
-        lighter: 'var(--color-theme-lighter)',
-        default: 'var(--color-theme)',
+    extend: {
+      colors: {
+        ...colors,
+        theme: {
+          lighter: 'var(--color-theme-lighter)',
+          default: 'var(--color-theme)',
+        },
       },
     },
   },
   variants: {},
   plugins: [],
-  purge: {
-    // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
-    enabled: process.env.NODE_ENV === 'production',
-    content: [
-      'components/**/*.vue',
-      'layouts/**/*.vue',
-      'pages/**/*.vue',
-      'plugins/**/*.js',
-      'nuxt.config.js',
-    ],
-  },
+  content: [
+    'components/**/*.vue',
+    'layouts/**/*.vue',
+    'pages/**/*.vue',
+    'plugins/**/*.js',
+    'nuxt.config.js',
+  ],
 }
